817fccb1dba8481fe3f868fbba6b2f1ab2836d62
Ahmad <akhal983@gmail.com>
2025-12-01 17:54:24 +0300

    update

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-
 echo "Waiting for MySQL to be available..."
-
 while ! nc -z db 3306; do
   sleep 1
-  echo "Waiting for MySQL..."
+  echo "Waiting..."
 done
-
 echo "MySQL is up!"
 exec "$@"
